47c7724b4fd5652f4fdf4b592ab77ced0c9cd059
Luis CS <lewsdev@gmail.com>
2022-07-08 16:38:04 -0500

    Cambios en el home, ahora presenta 2 botones para agragar productos y listar productos

--- app/products/routes.py
+++ app/products/routes.py
@@ -5,5 +5,9 @@ from app.products import products
 
 @products.route('/products')
 def list_products():
-    return render_template('products/products.html')
+    return render_template('products/list_products.html')
 
+
+@products.route('/add_products')
+def add_products():
+    return render_template('products/add_products.html')
