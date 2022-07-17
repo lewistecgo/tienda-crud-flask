e40478e0d51baa70af17e8e255dff02cd426cab7
Luis CS <lewsdev@gmail.com>
2022-07-17 13:27:32 -0500

    listar productos de la base de datos y mejoras menores

--- app/products/routes.py
+++ app/products/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template
 
 from app import db
 from app.products import products
@@ -10,12 +10,13 @@ from models import Products
 def list_products():
     productos = Products.get_by_id(1)
     print(productos)
-    return render_template('products/list_products.html')
+    list_prod = Products.query.all()
+
+    return render_template('products/list_products.html', list_prod=list_prod)
 
 
 @products.route('/add_products', methods=['POST', 'GET'])
 def add_products():
-
     products_form = AddProductForm()
     if products_form.validate_on_submit():
         name = products_form.name.data
@@ -24,7 +25,8 @@ def add_products():
         price = products_form.price.data
         tag = products_form.tag.data
 
-        new_product = Products(product_name=name, product_description = description, product_quantity=quantity, product_price=price,product_tag=tag)
+        new_product = Products(product_name=name, product_description=description, product_quantity=quantity,
+                               product_price=price, product_tag=tag)
         db.session.add(new_product)
         db.session.commit()
 
